a515a7be202b27df1014786b1fc724822028f09e
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-12-01 12:23:16 +0300

    Added header scroll transition

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
 const Header = () => {
+	const [prevScrollPos, setPrevScrollPos] = useState(0);
+	const [visible, setVisible] = useState(true);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const currentScrollPos = window.scrollY;
+			const isScrollingUp = prevScrollPos > currentScrollPos;
+
+			setPrevScrollPos(currentScrollPos);
+
+			// Set the visibility based on the scroll direction
+			setVisible((prevVisible) => (isScrollingUp || currentScrollPos < 10 ? true : false));
+		};
+
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, [prevScrollPos]);
+
 	return (
 		<>
-			<header>
+			<header className={visible ? 'visible' : 'hidden'}>
 				<nav>
 					<button>About</button>
 					<button>Projects</button>
