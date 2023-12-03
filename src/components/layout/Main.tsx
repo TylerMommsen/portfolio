fdfdaa005e654472ef72fa62b4e2ec1be2ed2ee9
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-12-03 14:18:24 +0300

    Added hero section text and view projects button

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -2,11 +2,13 @@ import Hero from '../sections/Hero';
 import Projects from '../sections/Projects';
 import About from '../sections/About';
 import Contact from '../sections/Contact';
+import Bg from '../common/bg';
 
 const Main = () => {
 	return (
 		<>
 			<main>
+				<Bg />
 				<Hero />
 				<About />
 				<Projects />
