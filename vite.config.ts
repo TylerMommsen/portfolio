c80cc6adae6df995bf061cb7b2a04f1ede73ddb5
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2024-02-09 20:11:51 +0300

    Removed some packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ViteSassPlugin from 'vite-plugin-sass';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: '/portfolio/',
-	plugins: [react(), ViteSassPlugin()],
+	plugins: [react()],
 });
